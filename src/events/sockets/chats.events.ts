336020e3a5f7955f8d76f8fee9840a8226d71bcf
Jaga <jagadheesh6@gmail.com>
2023-01-22 13:31:09 +0530

    test - socket chat events

--- src/events/sockets/chats.events.ts
+++ src/events/sockets/chats.events.ts
@@ -12,13 +12,13 @@ export class ChatEventsHandler {
 
     		/// Join room for DM and Group
 			socket.on('private_chat', async (data) => {
-				console.log('Join private room')
+				console.log('Join private room', data)
 				const { _id } = data
 				socket.join(_id)
 			});
 
 			socket.on('send_private_message', async (data) => {
-				console.log('Private message')
+				console.log('Private message', data)
 				const { _id, message } = data;
 				socket.to(_id).emit('receive_private_message', message);
 			});
